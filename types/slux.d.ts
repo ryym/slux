3dd0f04ac1cee894dcc9b70a616bd0b26b94fdc2
ryym <ryym.64@gmail.com>
2017-01-02 19:39:10 +0900

    feat(ts): Add TypeScript type definition

---
+++ types/slux.d.ts
@@ -0,0 +1,351 @@
+declare module 'slux' {
+    export type GetterFunc0<S, GX, R> = (s: S, c: GX) => R;
+    export type GetterFunc1<S, GX, T, R> = (s: S, c: GX, arg: T) => R;
+
+    export interface Getter0<S, GX, R> {
+        exec: (s: S, c: GX) => R;
+    }
+    export interface Getter1<S, GX, T, R> {
+        exec: (s: S, c: GX, arg: T) => R;
+    }
+
+    export type MutationFunc0<S, CX> = (s: S, c: CX) => S;
+    export type MutationFunc1<S, CX, T> = (s: S, c: CX, t: T) => S;
+
+    export interface Mutation0<S, CX> {
+        type: string;
+        exec: (s: S, c: CX) => S;
+    }
+    export interface Mutation1<S, CX, T> {
+        type: string;
+        exec: (s: S, c: CX, t: T) => S;
+    }
+
+    export type ActionFunc0<DX, R> = (c: DX) => R;
+    export type ActionFunc1<DX, T, R> = (c: DX, t: T) => R;
+
+    export interface Action0<DX, R> {
+        type: string;
+        exec: (c: DX) => R;
+    }
+    export interface Action1<DX, T, R> {
+        type: string;
+        exec: (c: DX, t: T) => R;
+    }
+
+
+    export interface Query<S, GX> {
+        <R>(getter: Getter0<S, GX, R>): R;
+        <T, R>(getter: Getter1<S, GX, T, R>, arg: T): R;
+    }
+    export interface Commit<S, CX> {
+        (mutation: Mutation0<S, CX>): S;
+        <T>(mutation: Mutation1<S, CX, T>, arg: T): S;
+    }
+    export interface Run<DX> {
+        <R>(action: Action0<DX, R>): R;
+        <T, R>(action: Action1<DX, T, R>, arg: T): R;
+    }
+
+    export type GetterContext<S> = {
+        query: SingleQuery<S>
+    };
+    export type MutationContext<S> = {
+        query: SingleQuery<S>,
+        commit: SingleCommit<S>
+    };
+    export type ActionContext<S> = {
+        query: SingleQuery<S>,
+        commit: SingleCommit<S>,
+        run: SingleDispatch<S>
+    };
+
+    type SingleQuery<S> = Query<S, GetterContext<S>>;
+    type SingleCommit<S> = Commit<S, MutationContext<S>>;
+    type SingleDispatch<S> = Run<ActionContext<S>>;
+
+    type MutationData = {
+        type: string;
+        payload: any;
+        includes: MutationData[];
+    };
+    export class Store<S, GX, CX, DX, Stores, Snap> {
+        query: Query<S, GX>;
+        commit: Commit<S, CX>;
+        run: Run<DX>;
+        getState(): S;
+        takeSnapshot(): Snap;
+        onMutation(handler: (data: MutationData, store: Store<S, GX, CX, DX, Stores, Snap>) => void): void;
+    }
+
+    export class SingleStore<S, Snap> extends Store<
+        S,
+        GetterContext<S>,
+        MutationContext<S>,
+        ActionContext<S>,
+        {},
+        Snap
+    > {}
+
+    export function createStore<S, Snap>(config: {
+        name: string,
+        getInitialState: () => S,
+        takeSnapshot?: (state: S) => Snap,
+    }): SingleStore<S, Snap>;
+
+    export interface CombinedQuery {
+        <S, G, R>(
+            store: SealedStore<S, G, any, any, any, any>,
+            getter: Getter0<S, G, R>
+        ): R;
+        <S, G, T, R>(
+            store: SealedStore<S, G, any, any, any, any>,
+            getter: Getter1<S, G, T, R>,
+            arg: T
+        ): R;
+    }
+    export interface CombinedCommit {
+        <S, C>(
+            store: SealedStore<S, any, C, any, any, any>,
+            mutation: Mutation0<S, C>
+        ): S;
+        <S, C, T>(
+            store: SealedStore<S, any, C, any, any, any>,
+            mutation: Mutation1<S, C, T>,
+            arg: T
+        ): S;
+    }
+    export interface CombinedRun {
+        <S, D, R>(
+            store: SealedStore<any, any, any, D, any, any>,
+            action: Action0<D, R>
+        ): R;
+        <S, D, T, R>(
+            store: SealedStore<any, any, any, D, any, any>,
+            action: Action1<D, T, R>,
+            arg: T
+        ): R;
+    }
+
+    export type CombinedGetterContext<S, Stores> = {
+        query: CombinedQuery,
+        stores: { self: CombinedSealedStore<S, Stores, any> } & Stores
+    };
+    export type CombinedMutationContext<S, Stores> = {
+        query: CombinedQuery,
+        commit: CombinedCommit,
+        stores: { self: CombinedSealedStore<S, Stores, any> } & Stores
+    };
+    export type CombinedActionContext<S, Stores> = {
+        query: CombinedQuery,
+        commit: CombinedCommit,
+        run: CombinedRun,
+        stores: { self: CombinedSealedStore<S, Stores, any> } & Stores
+    };
+
+    export class CombinedStore<S, Stores, Snap> extends Store<
+        S,
+        CombinedGetterContext<S, Stores>,
+        CombinedMutationContext<S, Stores>,
+        CombinedActionContext<S, Stores>,
+        Stores,
+        Snap
+    > {
+        withSubs(process: (
+            stores: Stores,
+            context: {
+                query: CombinedQuery,
+                commit: CombinedCommit,
+                run: CombinedRun,
+            }
+        ) => void): void;
+    }
+
+    type SealedStores = {
+        [key: string]: SealedStore<any, any, any, any, any, any>
+    };
+    export function combineStores<S, Stores extends SealedStores, Snap>(
+        config: {
+            name: string,
+            getInitialState: () => S,
+            takeSnapshot?: (state: S, stores: Stores) => Snap,
+            stores: (sub: Sealer) => Stores
+        }
+    ): CombinedStore<S, Stores, Snap>;
+
+    export class SealedStore<S, G, C, D, Stores, Snap> {
+        constructor(store: Store<S, G, C, D, Stores, Snap>);
+        takeSnapshot(): Snap;
+    }
+
+    type SingleSealedStore<S, Snap> = SealedStore<
+        S,
+        GetterContext<S>,
+        MutationContext<S>,
+        ActionContext<S>,
+        {},
+        Snap
+    >;
+
+    type CombinedSealedStore<S, Stores, Snap> = SealedStore<
+        S,
+        CombinedGetterContext<S, Stores>,
+        CombinedMutationContext<S, Stores>,
+        CombinedActionContext<S, Stores>,
+        Stores,
+        Snap
+    >;
+
+    type Sealer = <S, G, C, D, Stores, Snap>(
+        store: Store<S, G, C, D, Stores, Snap>
+    ) => SealedStore<S, G, C, D, Stores, Snap>;
+
+
+    export function getter<F extends GetterFunc0<any, any, any>>(f: F): { exec: F };
+    export function getter<F extends GetterFunc1<any, any, any, any>>(f: F): { exec: F };
+
+    export function getterWith<Dep, F extends GetterFunc0<any, any, any>>(
+        dep: Dep,
+        g: (dep: Dep) => F
+    ): { exec: F, with: (dep: Dep) => F };
+    export function getterWith<Dep, F extends GetterFunc1<any, any, any, any>>(
+        dep: Dep,
+        g: (dep: Dep) => F
+    ): { exec: F, with: (dep: Dep) => F };
+
+    export function mutation<F extends MutationFunc0<any, any>>(
+        type: string,
+        f: F
+    ): { type: string, exec: F };
+    export function mutation<F extends MutationFunc1<any, any, any>>(
+        type: string,
+        f: F
+    ): { type: string, exec: F };
+
+    export function mutationWith<Dep, F extends MutationFunc0<any, any>>(
+        dep: Dep,
+        type: string,
+        m: (dep: Dep) => F
+    ): { type: string, exec: F, with: (dep: Dep) => F };
+    export function mutationWith<Dep, F extends MutationFunc1<any, any, any>>(
+        dep: Dep,
+        type: string,
+        m: (dep: Dep) => F
+    ): { type: string, exec: F, with: (dep: Dep) => F };
+
+    export function action<F extends ActionFunc0<any, any>>(
+        type: string,
+        a: F
+    ): { type: string, exec: F };
+    export function action<F extends ActionFunc1<any, any, any>>(
+        type: string,
+        a: F
+    ): { type: string, exec: F };
+
+    export function actionWith<Dep, F extends ActionFunc0<any, any>>(
+        dep: Dep,
+        type: string,
+        a: (dep: Dep) => F
+    ): { type: string, exec: F, with: (dep: Dep) => F };
+    export function actionWith<Dep, F extends ActionFunc1<any, any, any>>(
+        dep: Dep,
+        type: string,
+        a: (dep: Dep) => F
+    ): { type: string, exec: F, with: (dep: Dep) => F };
+
+
+    export type Command0 = {
+        type: string
+    };
+    export type Command1<T> = {
+        type: string,
+        payload: T
+    };
+
+    interface CommitMaker<S, CX> {
+        (mutation: Mutation0<S, CX>): () => Command0;
+        <T>(mutation: Mutation1<S, CX, T>): (t: T) => Command1<T>;
+    }
+
+    interface DispatchMaker<S, DX> {
+        (action: Action0<DX, any>): () => Command0;
+        <T>(action: Action1<DX, T, any>): (t: T) => Command1<T>;
+    }
+
+    export interface Dispatch {
+        (c: () => Command0): void;
+        <T>(c: (t: T) => Command1<T>, arg: T): void;
+    }
+    export class Dispatcher {
+        dispatch: Dispatch;
+    }
+
+    export function createDispatcher<S, G, C, D, CM>(
+        store: Store<S, G, C, D, any, any>,
+        defineCommands: (
+            commit: CommitMaker<S, C>,
+            run: DispatchMaker<S, D>
+        ) => CM
+    ): {
+        dispatcher: Dispatcher,
+        commands: CM
+    };
+
+    interface CommitMakerWithStore {
+        <S, CX>(store: Store<S, any, CX, any, any, any>, mutation: Mutation0<S, CX>): () => Command0;
+        <S, CX, T>(store: Store<S, any, CX, any, any, any>, mutation: Mutation1<S, CX, T>): (t: T) => Command1<T>;
+    }
+
+    interface DispatchMakerWithStore {
+        <S, DX>(store: Store<S, any, any, DX, any, any>, action: Action0<DX, any>): () => Command0;
+        <S, DX, T>(store: Store<S, any, any, DX, any, any>, action: Action1<DX, T, any>): (t: T) => Command1<T>;
+    }
+
+    export function createCombinedDispatcher<S, G, C, D, CM>(
+        store: Store<S, G, C, D, any, any>,
+        defineCommands: (
+            commit: CommitMakerWithStore,
+            run: DispatchMakerWithStore
+        ) => CM
+    ): {
+        dispatcher: Dispatcher,
+        commands: CM
+    };
+}
+
+declare module 'slux/getters' {
+    export let getInitialState: {
+        exec<S>(state: S, context: any): S;
+    };
+    export let getState: {
+        exec<S>(state: S, context: any): S;
+    };
+}
+
+declare module 'slux/react' {
+    import { Query, Dispatch, Dispatcher, CombinedQuery, Sealer } from 'slux';
+    import { Component, ComponentClass, StatelessComponent } from 'react';
+
+    type ReactComponent<P> = ComponentClass<P> | StatelessComponent<P>;
+
+    export function connect<P1, P2, WP>(
+        mapStateToProps: (query: Query<any, any>, props: WP) => P1,
+        mapDispatchToProps?: (dispatch: Dispatch) => P2
+    ): (component: ReactComponent<P1 & P2>) => ComponentClass<WP>;
+
+    interface ProviderProps {
+        dispatcher: Dispatcher;
+    }
+    export class Provider extends Component<ProviderProps, {}> {}
+
+    export type CustomQuery = CombinedQuery;
+
+    interface CustomConnect<Stores> {
+        <P1, P2, WP>(
+            mapStateToProps: (query: CustomQuery, stores: Stores, props: WP) => P1,
+            mapDispatchToProps?: (dispatch: Dispatch) => P2
+        ): (component: ReactComponent<P1 & P2>) => ComponentClass<WP>;
+    }
+
+    export function createConnector<Stores>(defineStores: (seal: Sealer) => Stores): CustomConnect<Stores>;
+}
